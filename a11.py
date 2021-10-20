3ac742b0330b5b6d735311a45acc987994366083
swethamani25 <92358649+swethamani25@users.noreply.github.com>
2021-10-20 12:37:35 +0530

    Add files via upload

---
+++ a11.py
@@ -0,0 +1,22 @@
+from HTMLParser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data):
+        if '\n' not in data:
+            print('>>> Single-line Comment')
+            print(data)
+        elif '\n' in data:
+            print('>>> Multi-line Comment')
+            print(data)
+    def handle_data(self, data):
+        if data != '\n':
+            print('>>> Data')
+            print(data)  
+html = ""       
+for i in range(int(raw_input())):
+    html += raw_input().rstrip()
+    html += '\n'
+    
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
